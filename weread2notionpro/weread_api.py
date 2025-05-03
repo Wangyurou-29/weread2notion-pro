44d55dcf6ec3e026e624eb3a0f543ed17c78c100
Wangyurou-29 <52941111+Wangyurou-29@users.noreply.github.com>
2025-05-03 18:02:30 +0800

    Update weread_api.py

--- weread2notionpro/weread_api.py
+++ weread2notionpro/weread_api.py
@@ -12,8 +12,9 @@ from dotenv import load_dotenv
 load_dotenv()
 WEREAD_URL = "https://weread.qq.com/"
 WEREAD_NOTEBOOKS_URL = "https://i.weread.qq.com/user/notebooks"
-WEREAD_BOOKMARKLIST_URL = "https://i.weread.qq.com/book/bookmarklist"
-WEREAD_CHAPTER_INFO = "https://i.weread.qq.com/book/chapterInfos"
+//WEREAD_BOOKMARKLIST_URL = "https://i.weread.qq.com/book/bookmarklist"  
+WEREAD_BOOKMARKLIST_URL = "	https://weread.qq.com/web/shelf/sync"      
+WEREAD_CHAPTER_INFO = "https://i.weread.qq.com/book/chapterInfos"      
 WEREAD_READ_INFO_URL = "https://i.weread.qq.com/book/readinfo"
 WEREAD_REVIEW_LIST_URL = "https://i.weread.qq.com/review/list"
 WEREAD_BOOK_INFO = "https://i.weread.qq.com/book/info"
